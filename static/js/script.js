dc2e7d5861e02feb7ba86d8c77c5c974b0e12046
tlawxp <tlawme15@gmail.com>
2023-03-25 20:29:37 +0700

    portofolio

---
+++ static/js/script.js
@@ -0,0 +1,73 @@
+let btnOpen = document.getElementById("btnOpen");
+let btnClose = document.getElementById("btnClose");
+let menuBars = document.getElementById("menuBars");
+
+btnOpen.onclick = function () {
+  menuBars.style.display = "block";
+  menuBars.style.animation = "btnOpen 0.5s ease forwards alternate";
+};
+btnClose.onclick = function () {
+  menuBars.style.animation = "btnClose 0.5s ease forwards alternate";
+};
+
+
+
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // ...............[ INJECT CSS ]...............
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML =
+    ".typewrite > .wrap { border-right: 0.08em solid #4e9; animation: animasietik 0.5s ease infinite alternate;} @keyframes animasietik { 50% { border-right: 0.08em solid #4e9;} 100% { border-right: 0.08em solid #000} }";
+  document.body.appendChild(css);
+};
